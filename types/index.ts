45a597e35f98813a3f75f3186626aee0d1595092
kosyy01 <alekswysok@gmail.com>
2026-08-15 10:34:25 +0200

    Update website

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,14 @@ export interface NavLink {
   href: string;
 }
 
+/** Obsługiwane języki: polski, angielski (UK/US) i niemiecki. */
+export type Locale = "pl" | "en-GB" | "en-US" | "de";
+
+export interface NavItem {
+  key: "start" | "oNas" | "uslugi" | "realizacje" | "faq" | "kontakt";
+  href: string;
+}
+
 export interface Point {
   x: number;
   y: number;
